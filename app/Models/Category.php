9d91d226207f06df9b1a6d99bdfff176f5d21451
Ahmad Syamsul Arifin <aarie33ahmad@gmail.com>
2022-06-02 16:48:20 +0700

    fix table relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,8 +10,11 @@ class Category extends Model
     use HasFactory;
     protected $guarded = [];
     protected $dates = ['created_at'];
+    protected $fillable = [
+        'name', 'description'
+    ];
 
-    public function product()
+    public function products()
     {
         return $this->hasMany(Product::class);
     }
